078a9a4f27fbc48c306be2cca452a0d642e53fda
Tausif Khan <tausif.ee@gmail.com>
2020-08-31 23:46:54 +0530

    node-express server to make stripe payment requests

--- server.js
+++ server.js
@@ -52,58 +52,66 @@ app.get('/checkout-session', async (req, res) => {
 app.post('/create-checkout-session', async (req, res) => {
   const domainURL = process.env.DOMAIN;
 
+  console.log('req.body: ', req.body);
   const { sizeAndQty, addressRequired, locale } = req.body;
+  // const { sizeAndQty, addressRequired } = reqBody;
   //Find Total Quantity
   const orderedItems = [
-    {
-      name: 'Tshirt',
-      currency: 'inr',
-      amount: 350,
-      quantity: 1,
-    },
-    {
-      name: 'shirt',
-      currency: 'inr',
-      amount: 300,
-      quantity: 1,
-    }
+    // {
+    //   name: 'Tshirt',
+    //   currency: 'inr',
+    //   amount: 350,
+    //   quantity: 1,
+    // },
+    // {
+    //   name: 'shirt',
+    //   currency: 'inr',
+    //   amount: 300,
+    //   quantity: 1,
+    // }
   ];
-  // for ( let proKey in sizeAndQty ) {
-  //     orderedItems.push(
-  //       {
-  //         name: 'Tshirt',
-  //         currency: 'inr',
-  //         amount: 350,
-  //         quantity: sizeAndQty[ proKey ].quantity,
-  //       }
-  //     );
-  // }
-  const shippingAddressCollection = null;
-  if( addressRequired ){
-    shippingAddressCollection = {
-      allowed_countries: ['IN'],
-    }
+
+  for (let proKey in sizeAndQty) {
+    orderedItems.push(
+      {
+        name: 'Tshirt',
+        currency: 'inr',
+        amount: 350 *100,
+        quantity: sizeAndQty[proKey].quantity,
+      }
+    );
   }
-  console.log(orderedItems, sizeAndQty, addressRequired, shippingAddressCollection);
+
   // Create new Checkout Session for the order
   // Other optional params include:
   // [billing_address_collection] - to display billing address details on the page
   // [customer] - if you have an existing Stripe Customer ID
   // [customer_email] - lets you prefill the email input in the Checkout page
   // For full details see https://stripe.com/docs/api/checkout/sessions/create
-  const session = await stripe.checkout.sessions.create(
-    {
-      payment_method_types: process.env.PAYMENT_METHODS.split(', '),
-      // billing_address_collection: 'addressRequired',
-      shipping_address_collection: shippingAddressCollection,
-      mode: 'payment',
-      locale: locale,
-      line_items: orderedItems,
-      success_url: `${domainURL}/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${domainURL}/canceled.html`,
-      // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
+  const checkoutObject = {
+    payment_method_types: process.env.PAYMENT_METHODS.split(', '),
+    // billing_address_collection: 'addressRequired',
+    mode: 'payment',
+    locale: locale,
+    line_items: orderedItems,
+    success_url: `${domainURL}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${domainURL}/canceled.html`,
+    // ?session_id={CHECKOUT_SESSION_ID} means the redirect will have the session ID set as a query param
 
-    }
+  };
+
+  //Add address field in Checkout if User has no address
+  if (addressRequired === 'required') {
+    checkoutObject[ 'shipping_address_collection' ] = {
+      allowed_countries: ['IN'],
+    };
+  }
+
+  console.log(orderedItems, sizeAndQty, addressRequired);
+
+
+  const session = await stripe.checkout.sessions.create(
+    checkoutObject
   ).catch(error => console.log(error.message));
   // console.log('session fetched on Server: ', session);
 
